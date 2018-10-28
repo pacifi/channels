ad165238e14bb423f29abd6837273d7f1fc3afbd
pacifi <pacifi.bnr@gmail.com>
2018-10-27 22:55:06 -0500

    mensajes

--- chat/views.py
+++ chat/views.py
@@ -3,8 +3,30 @@ import json
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 
+from django.shortcuts import HttpResponse
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+import channels
+from asgiref.sync import async_to_sync
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 def index(request):
+    room_name = 'chat'
+    group_name = 'chat_chat'
+
+    channel_layer = channels.layers.get_channel_layer()
+
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        {
+            'type': 'chat_message',
+            'message': "messagesssss"
+        }
+    )
     context = {}
     return render(request, "chat/index.html", context)
 
@@ -14,5 +36,3 @@ def room(request, room_name):
         'room_name_json': mark_safe(json.dumps(room_name))
     }
     return render(request, 'chat/room.html', context)
-
-
